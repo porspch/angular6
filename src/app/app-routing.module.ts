561e79c47aa377a4b3aad5ab02b764a495c841e9
Suppakrit <suppakrit.pra@gmail.com>
2020-09-09 11:10:07 +0700

    import bootstrap version 4.5.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent
   },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
